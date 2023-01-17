f161b2f9380b8a23ca270ad0a1a7389a69448fa7
wangchensheng <wangchensheng@wakedata.com>
2023-01-17 11:34:05 +0800

    feat:领域层生成代码框架测试类
    Change-Id: Iba7a3d6e236937f113bc4362c0bc5fec545cdb35

---
+++ wd-generator-core/src/test/java/com/wd/paas/generator/newdsl/input/dsl/DomainDslTest.java
@@ -0,0 +1,48 @@
+package com.wd.paas.generator.newdsl.input.dsl;
+
+import com.google.common.reflect.TypeToken;
+import com.wd.paas.generator.newdsl.generate.visitor.objectstruct.DslStruct;
+import com.wd.paas.generator.newdsl.generate.visitor.visitor.velocitytemplate.JavaTemplateVisitor;
+import com.wd.paas.generator.newdsl.generate.visitor.visitor.velocitytemplate.TemplateContext;
+import com.wd.paas.generator.newdsl.input.util.Dsl2JsonUtil;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+public class DomainDslTest {
+
+    @Test
+    public void buildApplication() throws IOException {
+        // DSL json 转 dsl
+        ApplicationDsl applicationDsl = Dsl2JsonUtil.getDslElement("./src/test/resources/applicationDsl.json", ApplicationDsl.class);
+        List<BusinessDomainDsl> businessDomainDslList = Dsl2JsonUtil
+                .getDslElement("./src/test/resources/businessDomainDsl.json", new TypeToken<List<BusinessDomainDsl>>(){}.getType());
+        List<AggregateDsl> aggregateDslList = Dsl2JsonUtil
+                .getDslElement("./src/test/resources/aggregationDsl.json", new TypeToken<List<AggregateDsl>>(){}.getType());
+        List<CommandDsl> commandDslList = Dsl2JsonUtil
+                .getDslElement("./src/test/resources/commandDsl.json", new TypeToken<List<CommandDsl>>(){}.getType());
+        List<EntityDsl> entityDslList = Dsl2JsonUtil
+                .getDslElement("./src/test/resources/entityDsl.json", new TypeToken<List<EntityDsl>>(){}.getType());
+        List<EnumDsl> enumDslList = Dsl2JsonUtil
+                .getDslElement("./src/test/resources/enumDsl.json", new TypeToken<List<EnumDsl>>(){}.getType());
+        List<ValueObjectDsl> valueObjectDslList = Dsl2JsonUtil
+                .getDslElement("./src/test/resources/valueObjectDsl.json", new TypeToken<List<ValueObjectDsl>>(){}.getType());
+
+        aggregateDslList.get(0).setEntityList(entityDslList);
+        aggregateDslList.get(0).setEnumList(enumDslList);
+        aggregateDslList.get(0).setValueObjectList(valueObjectDslList);
+        aggregateDslList.get(0).setCommandList(commandDslList);
+        businessDomainDslList.get(0).getDomainModel().setAggregateList(aggregateDslList);
+        applicationDsl.setBusinessDomainList(businessDomainDslList);
+
+        DslStruct dslStruct = new DslStruct();
+        dslStruct.add(applicationDsl.build());
+
+        JavaTemplateVisitor javaTemplateVisitor = new JavaTemplateVisitor(new TemplateContext("./",null));
+        dslStruct.accept(javaTemplateVisitor);
+
+        Assert.assertTrue(true);
+    }
+}
